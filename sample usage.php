c9a67a95a993440c6212ffba408c89a025ef83bc
Benjamin Maynor <BenjaminMaynor@gmail.com>
2011-02-17 15:01:55 -0600

    Updated the sample usage file.

--- sample usage.php
+++ sample usage.php
@@ -10,10 +10,17 @@ $pdf = new ZendPDF_Helper_Pdf();
 $table = $pdf->addTable(array('align'=>'justify'));
 $table->addSpacerRow();
 $table->addRow()
-          ->addCol("Evaluation", array('bold' => true, 'colspan' => 7, 'align' => 'center'));
+          ->addCol("Some Header", array('bold' => true, 'colspan' => 3, 'align' => 'center'));
 $table->addRow()
-          ->addCol("Right Carotid", array('bold' => true, 'align' => 'center', 'colspan'=>3))
+          ->addCol("Column Text 1", array('bold' => true, 'align' => 'center', 'colspan'=>3))
           ->addCol(" ")
-          ->addCol("Left Carotid", array('bold' => true, 'align' => 'center', 'colspan'=>3));
+          ->addCol("Column Text 2", array('bold' => true, 'align' => 'center', 'colspan'=>3));
+
+// Per popular request, an example of adding columns using a loop
+$row = $table->addRow();
+
+for(i = 0; i < $someConstraint; $i++) {
+    $row->addCol("Some Info");
+}
 
 $pdf->build();
